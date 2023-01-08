d7d5b001106352da9cbb6b771dcb80d236d559ce
MatiasCordich <m.scordich@gmail.com>
2023-01-07 23:48:01 -0300

    CRUD completed

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const { createTaskCtrl, getAllTasksCtrl, updateTaskCtrl, deleteTaskCtrl } = require('../controllers/task')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send({msg: 'Hola'})
-})
+router.post('/', createTaskCtrl)
+router.get('/all', getAllTasksCtrl)
+router.get('/myTasks', getAllTasksCtrl)
+router.put('/:taskId', updateTaskCtrl)
+router.delete('/:taskId', deleteTaskCtrl)
 
 module.exports = router
