edd502ea2e0a5230ddf15c1f1a792ef80caca4cd
MatiasCordich <m.scordich@gmail.com>
2023-01-12 10:26:41 -0300

    cors origin fiexed

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const corsOptions = {
 // Middlewares
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://mern-task-manager-frontend.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'true');
   next();
 });
 
