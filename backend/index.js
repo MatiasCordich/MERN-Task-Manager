a144751a2365dfc14a128c7981e047b257d5e4b5
MatiasCordich <m.scordich@gmail.com>
2023-01-12 09:29:36 -0300

    headers set

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,13 @@ app.use(morgan('tiny'))
 app.use(express.json())
 app.use(cookieParser())
 
+app.use((req,res,next)=>{
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+  next(); 
+})
+
 // Rutas
 
 app.use('/api', allRoutes)
